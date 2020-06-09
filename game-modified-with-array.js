8231d3f796a34639231eba8d7381ae2d3ef7c17b
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-06-09 15:14:08 -0400

    Make questions local - try 1

--- game-modified-with-array.js
+++ game-modified-with-array.js
@@ -30,7 +30,7 @@ let availableQuesions = [];
 
 
 
-//let questions = [];
+let questions = [];
 
 var sec = 75;
 var timer;
@@ -49,179 +49,127 @@ function setTime() {
     }
 
   }, 1000);
-};
-/*
- let questions = [{
-    question: "1. How do you write 'Hello World' in an alert box?",
-    choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
-    correctAnswer: 3
-}, {
-    question: "2. How to empty an array in JavaScript?",
-    choices: ["arrayList[]", "arrayList(0)", "arrayList.length=0", "arrayList.len(0)"],
-    correctAnswer: 2
-}, {
-    question: "3. What function to add an element at the begining of an array and one at the end?",
-    choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
-    correctAnswer: 1
-}]; */
- questions = [
+}
+availableQuestions = [
+
     {
 
-        question: "How do you write 'Hello World' in an alert box?",
+        "question": "Inside which HTML element do we put the JavaScript?",
 
-        choice1: "msgBox('Hello World');",
+        "choice1": "\x3Cscript\x3E",
 
-        choice2: "alertBox('Hello World');",
+        "choice2": "\x3Cjavascript\x3E",
 
-        choice3: "msg('Hello World');",
+        "choice3": "\x3Cjs\x3E",
 
-        choice4: "alert('Hello World');",
+        "choice4": "\x3Cscripting\x3E",
 
-        answer: 4
+        "answer": 1
 
     },
 
     {
 
-        question: "Which of the following is not a reserved word in JavaScript?",
+        "question": "What is the correct syntax for referring to an external script called 'xxx.js'?",
 
-        choice1: "interface",
+        "choice1": "\x3Cscript href='xxx.js'\x3E",
 
-        choice2: "throws",
+        "choice2": "\x3Cscript name='xxx.js'\x3E",
 
-        choice3: "program",
+        "choice3": "\x3Cscript src='xxx.js'\x3E",
 
-        choice4: "short",
+        "choice4": "\x3Cscript file='xxx.js'\x3E",
 
-        answer: 3
+        "answer": 3
 
     },
 
     {
 
-        question: "JavaScript is a ___ -side programming language",
+        "question": " How do you write 'Hello World' in an alert box?",
 
-        choice1: "Client",
+        "choice1": "msgBox('Hello World');",
 
-        choice2: "Server",
+        "choice2": "alertBox('Hello World');",
 
-        choice3: "Both",
+        "choice3": "msg('Hello World');",
 
-        choice4: "None",
+        "choice4": "alert('Hello World');",
 
-        answer: 3
+        "answer": 4
 
-    },
-
-
-    {
-
-        question: "What will the code return? Boolean (3 < 7)",
-
-        choice1: "true",
-
-        choice2: "false",
-
-        choice3: "NaN",
-
-        choice4: "SyntaxError",
-
-        answer: 1
-
-    },
-
-
-    {
-
-        question: "Which JavaScript label catches all the values, except for the ones specified?",
-
-        choice1: "catch",
-
-        choice2: "label",
-
-        choice3: "try",
-
-        choice4: "default",
-
-        answer: 4
-
-    },
-    {
-        question: "Which built-in method removes the last element from an array and returns that element?",
-
-        choice1: "last()",
-        choice2: "get()",
-        choice3: "pop()",
-        choice4: "None of the above",
-    
-        Answer: 3
-    },
-    {
-        question: "Which of the following function of String object causes a string to be italic, as if it were in an <i> tag?",
-    
-        choice1: "fixed()",
-        choice2: "fontcolor()",
-        choice3: "fontsize()",
-        choice4: "italics()",
-    
-        Answer: 4
     }
 
- ];
+];
+/*
+fetch(
 
-// fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
-// .then(res => {
-//     return res.json();
-// })
-// .then(loadedQuestions => {
-//     console.log(loadedQuestions.results);
-//      questions = loadedQuestions.results.map(loadedQuestion => {
-//          const formattedQuestion = { question: loadedQuestion.question };
-//          const answerChoices = [...loadedQuestion.incorrect_answers];
-        
-//        formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
-//          answerChoices.splice( formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
-//         answerChoices.forEach((choice, index) => { formattedQuestion["choice" + (index + 1)] = choice; });
-//                  return formattedQuestion;
-//      });
-     
-     
-//      console.log(questions);
+    "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
 
-//      startGame();
-//      setTime();
-//  }).catch(err => { console.error(err); });
+)
 
- //questions = require('questions.json');
+.then(res => {
+console.log('res.json',res.json);
 
+    return res.json();
+
+})
+
+
+.then(loadedQuestions => {
+    console.log('LOADED QUESTIONS',loadedQuestions)
+    console.log(loadedQuestions.results);
+
+    questions = loadedQuestions.results.map(loadedQuestion => {
+
+        const formattedQuestion = {
+
+            question: loadedQuestion.question
+
+        };
+
+
+
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+
+        formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+
+        answerChoices.splice(
+
+            formattedQuestion.answer - 1,
+
+            0,
+
+            loadedQuestion.correct_answer
+
+        );
+
+
+
+        answerChoices.forEach((choice, index) => {
+
+
+            formattedQuestion["choice" + (index + 1)] = choice;
+
+        });
+
+
+        console.log(formattedQuestion);
+        return formattedQuestion;
+
+    });
+
+*/
  
-//  [
-//      {
-//         question: "What is the capital of Illinois",
-//         answer: 3,
-//         choice1: "Albany",
-//         choice2: "Miami",
-//         choice3: "Springfield",
-//         choice4: "Baltimore"
-//      },
-//      {
-//         question: "What is the capital of Illinois",
-//         answer: 3,
-//         choice1: "Albany",
-//         choice2: "Miami",
-//         choice3: "Springfield",
-//         choice4: "Baltimore"
-//      }
-//  ]
+    startGame();
+    setTime();
+/*})
 
-//startGame();
-//setTime();
-// function init() {
-//     import questions from ('./questions.json');
-// }
-// //questions = require('./questions.json');
-// init();
+.catch(err => {
 
+    console.error(err);
+
+}); */
 /*
 var secondsLeft = 10;
 
@@ -238,11 +186,68 @@ function setTime() {
   }, 1000);
 } */
 
+
 //CONSTANTS
 
 const CORRECT_BONUS = 10;
 
 const MAX_QUESTIONS = 3;
+
+startGame = () => {
+
+    questionCounter = 0;
+
+    score = 0;
+
+  //  availableQuesions = [...questions];
+console.log('AVAILABLE QUESTIONS',availableQuesions);
+    getNewQuestion();
+
+    document.getElementById('timer').innerHTML = 'Time: ' +'00:' + sec;
+
+    game.classList.remove("hidden");
+
+    loader.classList.add("hidden");
+
+};
+/*
+var mydiv = document.getElementById("myDiv");
+var aTag = document.createElement('a');
+aTag.setAttribute('href',"yourlink.htm");
+aTag.innerText = "link text";
+mydiv.appendChild(aTag);
+
+vhsLink = document.createElement('a');
+vhsLink.setAttribute('href',"highscores.html");
+vhsLink.innerText = "View High Scores";
+
+
+
+*/
+function createVHSLink() {
+
+
+    // Create anchor element. 
+    let a = document.createElement('a');
+
+    // Create the text node for anchor element. 
+
+    let link = document.createTextNode("View High Scores");
+
+    // Append the text node to anchor element. 
+    a.appendChild(link);
+
+    // Set the title. 
+    /* a.title = "This is Link"; */
+
+    // Set the href property. 
+    a.href = "highscores.html";
+
+    // Append the anchor element to the body. 
+    //document.body.appendChild(a);
+    return a;
+}
+
 getNewQuestion = () => {
 
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
@@ -280,101 +285,6 @@ getNewQuestion = () => {
     acceptingAnswers = true;
 
 };
-
-
-startGame = () => {
-
-    questionCounter = 0;
-
-    score = 0;
-
-    availableQuesions = [...questions];
-
-    getNewQuestion();
-
-    document.getElementById('timer').innerHTML = 'Time: ' +'00:' + sec;
-
-    game.classList.remove("hidden");
-
-    loader.classList.add("hidden");
-
-};
-console.log("got here")
-startGame();
-setTime();
-/*
-var mydiv = document.getElementById("myDiv");
-var aTag = document.createElement('a');
-aTag.setAttribute('href',"yourlink.htm");
-aTag.innerText = "link text";
-mydiv.appendChild(aTag);
-
-vhsLink = document.createElement('a');
-vhsLink.setAttribute('href',"highscores.html");
-vhsLink.innerText = "View High Scores";
-*/
-
-function createVHSLink() {
-
-
-    // Create anchor element. 
-    let a = document.createElement('a');
-
-    // Create the text node for anchor element. 
-
-    let link = document.createTextNode("View High Scores");
-
-    // Append the text node to anchor element. 
-    a.appendChild(link);
-
-    // Set the title. 
-    /* a.title = "This is Link"; */
-
-    // Set the href property. 
-    a.href = "highscores.html";
-
-    // Append the anchor element to the body. 
-    //document.body.appendChild(a);
-    return a;
-}
-
-// getNewQuestion = () => {
-
-//     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-
-//         localStorage.setItem("mostRecentScore", sec);
-
-//         //go to the end page
-
-//         return window.location.assign("end.html");
-
-//     }
-
-//     questionCounter++;
-
-//     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-
-//     currentQuestion = availableQuesions[questionIndex];
-
-//     question.innerHTML = currentQuestion.question;
-
-
-
-//     choices.forEach(choice => {
-
-//         const number = choice.dataset["number"];
-
-//         choice.innerHTML = currentQuestion["choice" + number];
-
-//     });
-
-
-
-//     availableQuesions.splice(questionIndex, 1);
-
-//     acceptingAnswers = true;
-
-// };
 
 
 
